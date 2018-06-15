eec2d345074bc5861ce6176ec5b460934a73fa67
pradip9102 <pradip9102@gmail.com>
2018-06-15 21:11:32 +0530

    Tryout: After add-parameter.md

--- src/main/java/com/vizurd/tryout/refactoring/practice/MyCalendar.java
+++ src/main/java/com/vizurd/tryout/refactoring/practice/MyCalendar.java
@@ -9,7 +9,26 @@ public class MyCalendar {
     private Set<Appointment> appointments;
     private Kent kent = new Kent();
 
+    /**
+     * Finds all the appointments for the specified date.
+     *
+     * @param date the date for which appointments are to be found
+     * @return the list of appointments
+     * @deprecated Use {@link #findAppointments(LocalDate, String)} instead
+     */
+    @Deprecated
     private List<Appointment> findAppointments(LocalDate date) {
+        return findAppointments(date, "");
+    }
+
+    /**
+     * Finds all the appointments for the specified date.
+     *
+     * @param date the date for which appointments are to be found
+     * @param name the visitor name
+     * @return the list of appointments
+     */
+    private List<Appointment> findAppointments(LocalDate date, String name) {
         List<Appointment> result = new ArrayList<>();
         for (Appointment appointment : kent.getCourses()) {
             if (date.compareTo(appointment.getDate()) == 0) {
